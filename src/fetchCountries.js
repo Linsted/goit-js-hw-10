a2183d921285558cd8e361efe0fae5410a2e0004
Ivan Malenko <ivan.malenko18@gmail.com>
2023-01-12 08:44:19 +0200

    added markup for 1 country

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,13 @@
 export { fetchCountries };
+import Notiflix from 'notiflix';
 
 function fetchCountries(name) {
     const BASE_URL = `https://restcountries.com/v3.1/name/`;
   
    return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`)
        .then(response => {
-           if (!response.ok) { throw new Error };
+           
+           if(response.status === 404 && !response.ok) { throw new Error }
            
            return response.json()
        })
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries'
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500;
+let markUp = ``;
 
 const refs = {
-    input: document.querySelector('#search-box')
+    input: document.querySelector('#search-box'),
+    ul: document.querySelector(`.country-list`)
 }
 
 
@@ -13,14 +16,31 @@ refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY))
 
 function onInput() {
     const requestedCountry = refs.input.value.trim();
-    if (!requestedCountry) {return}
-    
+    if (!requestedCountry ) {return}
     fetchCountries(requestedCountry)
-        .then(data => console.log(data))
-       .catch(error => console.log(`We've got an`, error));
+        .then(data => {
+            console.log(data)
+            markUpCountries(data)})
+        .catch(error => Notiflix.Notify.failure('Oops, there is no country with that name'));
+    
 }
 
 
-console.log(2);
+console.log(3);
 
-
+function markUpCountries(arrayOfCountries) {
+    // const {}
+    
+    if (arrayOfCountries.length > 10)
+    {Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    }else  if (arrayOfCountries.length === 1) {
+        markUp = arrayOfCountries.map(({ capital, flags, languages, name, population}) => `<li><div class="wrapper">
+        <img src="${flags.svg}" alt="Country flag" width="40" height='40' /><h1>${name.official}</h1></div>
+        <p><b>Capital:</b> ${capital}</p>
+        <p><b>Population:</b> ${population}</p>
+        <p><b>Languages:</b> ${Object.values(languages)}</p>
+      </li>`).join(``)
+        return refs.ul.innerHTML = markUp;
+    }
+    
+} 
